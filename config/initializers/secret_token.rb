637da095899c6042514d9e9d76769b4b67961779
Matt Snyder <snyder2112@me.com>
2013-08-06 16:08:27 -0400

    Initialize rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fuey::Application.config.secret_key_base = '5bd6cfd6cad15ea80d49485d9427067b82aaf5465b63b8f1f8da037a04a29609a6a012b9e22f837f6c98c51d60a52dedb1153a91e1a811bf04f5ffc8c4b09f82'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fuey::Application.config.session_store :cookie_store, key: '_fuey_session'
