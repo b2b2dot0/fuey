4a2658b350b3632a9a3756e82803eecdf4ef1c54
Matt Snyder <snyder2112@me.com>
2013-08-22 08:23:27 -0400

    Show current status message for trace

--- app/assets/javascripts/trace_list_ctrl.js
+++ app/assets/javascripts/trace_list_ctrl.js
@@ -1,36 +1,21 @@
 function TraceListCtrl($scope) {
-  $scope.traces_fake = [
-    { 
-      "name": "CND Trace",
-      "steps": [
-        {
-          "type": "Ping",
-          "name": "Google DNS",
-          "settings": "8.8.8.8",
-          "status": "passed",
-          "statusMessage": "Ping succeeded." 
-        }
-      ]
-    },
-    { "name": "TMG Trace" }
-  ];    
+    $scope.orderProp = 'name';
+    $scope.traces = {};
 
-  $scope.orderProp = 'name';
-
-  $scope.traces = {};
-
-  var source = new EventSource('/traces');
-    source.addEventListener('new', function(e) {   
-      $scope.$apply(function() {        
-        var trace = JSON.parse(e.data);
-        $scope.traces[trace.name] = trace;
-      });
-     }, false);
-     source.addEventListener('update', function(e) {   
-      $scope.$apply(function() {        
-        var trace = JSON.parse(e.data);
-        var steps = $scope.traces[trace.name].steps;
-        $scope.traces[trace.name].steps[stepIndex(steps, trace.steps[0].name)] = trace.steps[0];
-      });
+    // Wire up SSE
+    var source = new EventSource('/traces');
+    source.addEventListener('new', function(e) {
+        $scope.$apply(function() {
+            var trace = JSON.parse(e.data);
+            $scope.traces[trace.name] = trace;
+        });
+    }, false);
+    source.addEventListener('update', function(e) {
+        $scope.$apply(function() {
+            var trace = JSON.parse(e.data);
+            var steps = $scope.traces[trace.name].steps;
+            $scope.traces[trace.name].statusMessage = trace.steps[0].statusMessage;
+            $scope.traces[trace.name].steps[stepIndex(steps, trace.steps[0].name)] = trace.steps[0];
+        });
     }, false);
 }
