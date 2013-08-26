31e82def7823f59519d3d652a1464911fb944846
Matt Snyder <snyder2112@me.com>
2013-08-26 14:46:56 -0400

    Set root to dahsboard#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Fuey::Application.routes.draw do
   get 'dashboard' => 'dashboard#index', :as => "dashboard"
   get 'traces' => 'traces#stream', :as => "stream_traces"
+  root 'dashboard#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
